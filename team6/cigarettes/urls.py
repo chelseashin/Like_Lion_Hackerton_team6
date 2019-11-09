a11919515e4524420a136658ea2bcb829a9dc58c
leejunghyun <cxz9080@gmail.com>
2019-11-10 01:37:34 +0900

    담배추가 기능 구현

--- team6/cigarettes/urls.py
+++ team6/cigarettes/urls.py
@@ -5,4 +5,8 @@ app_name = 'cigarettes'
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('newCigarette/', views.new_cigarette, name="new_cigarette"),
+    path('newElecCigarette/', views.new_elec_cigarette, name="new_elec_cigarette"),
+    path('createCigarette/', views.create_cigarette, name="create_cigarette"),
+    path('createElecCigarette/', views.create_elec_cigarette, name="create_elec_cigarette"),
 ]
--- team6/cigarettes/views.py
+++ team6/cigarettes/views.py
@@ -1,7 +1,7 @@
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
-from .models import Tobacco
+from .models import Tobacco, Cigarettes, ElecCigarettes
 from accounts.models import Profile
 
 # Create your views here.
@@ -32,7 +32,39 @@ def like(request, tobacco_id):
     return redirect('tobacco:detail', tobacco_id)
 
 def new_cigarette(request):
-    return render(request, 'cigarettes/create_tobacco.html')
+    tobacco_type = "cigarette"
+    return render(request, 'cigarettes/create_tobacco.html', {'tobacco_type' : tobacco_type})
+
+def new_elec_cigarette(request):
+    tobacco_type = "elec_cigarette"
+    return render(request, 'cigarettes/create_tobacco.html', {'tobacco_type' : tobacco_type})
 
 def create_cigarette(request):
-    return render(request, 'cigarettes/create_tobacco.html')
+    cigarette = Cigarettes()
+    cigarette.is_local = request.POST.get('is_local')
+    cigarette.brand = request.POST.get('brand')
+    cigarette.name = request.POST.get('name')
+    cigarette.price = request.POST.get('price')
+    cigarette.TAR = request.POST.get('tar')
+    cigarette.nicotine = request.POST.get('nicotine')
+    cigarette.rel_date = request.POST.get('rel_date')
+    cigarette.is_menthol = request.POST.get('is_menthol')
+    cigarette.save()
+
+    #return redirect('//' +str(new_post.id))
+    return redirect('cigarettes:index')
+
+def create_elec_cigarette(request):
+    elec_cigarette = ElecCigarettes()
+    elec_cigarette.c_type = request.POST.get('c_type')
+    elec_cigarette.brand = request.POST.get('brand')
+    elec_cigarette.name = request.POST.get('name')
+    elec_cigarette.price = request.POST.get('price')
+    elec_cigarette.TAR = request.POST.get('tar')
+    elec_cigarette.nicotine = request.POST.get('nicotine')
+    elec_cigarette.rel_date = request.POST.get('rel_date')
+    
+    elec_cigarette.save()
+
+    #return redirect('//' +str(new_post.id))
+    return redirect('cigarettes:index')
