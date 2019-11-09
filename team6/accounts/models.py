712c9dc75250b55b2a64c64c44c849b88bad08e9
leejunghyun <cxz9080@gmail.com>
2019-11-09 22:07:49 +0900

    signup/login/logout 구현.

--- team6/accounts/models.py
+++ team6/accounts/models.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    username = models.CharField(max_length=30)
+    nickname = models.CharField(max_length=30)
     email = models.TextField()
-    is_superuser = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
     
     def __str__(self):
         return self.user.username
